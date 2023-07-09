499e24fa9cb6b75e285f3b4dab06e47d3de6c960
Michael Vincent Franco <135952174+mikefrancozxc@users.noreply.github.com>
2023-07-10 01:30:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 
 mapboxgl.accessToken = "pk.eyJ1IjoibWlrZWZyYW5jb3EiLCJhIjoiY2xqOHg4ZGR6MHU3aTNzcXBqNm1vemV5YiJ9.lek5_593XJy1bE-UjhzgwA";
 
